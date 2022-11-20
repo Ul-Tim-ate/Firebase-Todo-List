4d1e57f9068ec697d814008981760992487c22a5
Timofey Trakhanov <timohator1@mail.ru>
2022-11-20 16:42:45 +0300

    create auth page

--- src/components/auth/auth.jsx
+++ src/components/auth/auth.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "../../css/auth/auth.css";
 
 const Auth = () => {
-  return <div className="auth">utads</div>;
+  return (
+    <div className="auth">
+      <button className="auth__btn">Авторизоваться через Google</button>
+    </div>
+  );
 };
 
 export default Auth;
