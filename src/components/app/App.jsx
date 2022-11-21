ce9e9ea0592337a9a4666ada13b7eb14b911ab81
Timofey Trakhanov <timohator1@mail.ru>
2022-11-21 22:39:14 +0300

    add loading spinner

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -4,18 +4,30 @@ import "../../css/app/App.css";
 import "../../css/app/reset.css";
 import TodoPage from "../todo-page/todo-page";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
+import Spinner from "../spinner/spinner";
 
 function App() {
   const auth = getAuth();
   const [user, setUser] = useState();
+  const [isLoading, setIsLoading] = useState(true);
   onAuthStateChanged(auth, (user) => {
+    console.log(user);
+
     if (user) {
       setUser(user.uid);
+      setIsLoading(false);
     } else {
       setUser(null);
+      setIsLoading(false);
     }
   });
-
+  if (isLoading) {
+    return (
+      <div className="app__loading">
+        <Spinner />
+      </div>
+    );
+  }
   return <div className="app">{user ? <TodoPage /> : <Auth />}</div>;
 }
 
