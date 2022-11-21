317831d23c54ca203b3c33cbf20b7caba74e0843
Timofey Trakhanov <timohator1@mail.ru>
2022-11-21 20:22:03 +0300

    add signout

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,11 +1,17 @@
+import { getAuth } from "firebase/auth";
 import React from "react";
 import "../../css/header/header.css";
 
 const Header = () => {
+  const signout = () => {
+    getAuth().signOut();
+  };
   return (
     <header className="header">
       <div className="container">
-        <button className="header__logout">Выйти</button>
+        <button className="header__logout" onClick={signout}>
+          Выйти
+        </button>
       </div>
     </header>
   );
